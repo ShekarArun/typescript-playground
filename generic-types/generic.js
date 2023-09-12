c35b51595437deb8b6cd59ad82044014b4bca09d
ShekarArun <shekar.arun@gmail.com>
2023-09-12 12:40:29 +0530

    fix: Compile generic script

---
+++ generic-types/generic.js
@@ -0,0 +1,18 @@
+/*
+Generic types allow for resuability of code
+eg: React can have unlimited number of components, and identifying types by inheriting base classes is impractical
+*/
+// Below component has color property because it is generic with Props1
+var comp1 = {
+    name: "First Component",
+    id: 1,
+    props: { color: "red" },
+};
+// Below component has size property because it is generic with Props2
+var comp2 = {
+    name: "Second component",
+    id: 2,
+    props: { size: 10 },
+};
+console.log(comp1);
+console.log(comp2);
