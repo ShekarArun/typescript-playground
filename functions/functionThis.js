549ba9190ee871db05ee53caea3d39fb4e5d6774
ShekarArun <shekar.arun@gmail.com>
2023-09-21 03:15:59 +0530

    feat: Add this keyword in function

---
+++ functions/functionThis.js
@@ -0,0 +1,10 @@
+var v1 = {
+    m1: ["Red", "Blue", "Green"],
+    m2: [1, 2, 3],
+    f1: function () {
+        return function () {
+            return this.m1[0]; // Note how without the specification of 'this' type in the line above, the type of 'this' and m1 here is 'any'
+        };
+    },
+};
+v1.f1();
---
+++ functions/functionThis.ts
@@ -0,0 +1,19 @@
+// Functions with 'this' are tricky in JS
+// TS helps control unexpected behaviour by allowing the specification of the type for 'this'
+interface i1 {
+  m1: string[];
+  m2: number[];
+  f1(): () => string;
+}
+
+let v1: i1 = {
+  m1: ["Red", "Blue", "Green"],
+  m2: [1, 2, 3],
+  f1: function () {
+    return function (this: i1) {
+      return this.m1[0]; // Note how without the specification of 'this' type in the line above, the type of 'this' and m1 here is 'any'
+    };
+  },
+};
+
+v1.f1();
