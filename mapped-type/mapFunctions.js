4bc651e36381f1432f4569b47fd0b90ed0287502
ShekarArun <shekar.arun@gmail.com>
2023-09-22 17:44:33 +0530

    feat: Add Pick Map function

---
+++ mapped-type/mapFunctions.js
@@ -0,0 +1,10 @@
+var cat1 = { age: 3, weight: 2 };
+var cat2 = { age: 1, weight: 1.5 };
+function buyAFish(fishEntity) {
+    console.log(fishEntity);
+}
+buyAFish({
+    age: 1,
+    name: "Clown Fish",
+    maximumDeepness: 10,
+});
--- mapped-type/mapFunctions.ts
+++ mapped-type/mapFunctions.ts
@@ -8,3 +8,28 @@ const cat1: Cat = { age: 3, weight: 2 };
 
 type Nullable<T> = { [P in keyof T]: T[P] | null };
 const cat2: Nullable<Cat> = { age: 1, weight: 1.5 };
+
+// Pick is a map function used to obtain a subset of properties from an interface with larger properties
+// This can also be achieved through inheritance but that approach has drawbacks
+interface Animal {
+  age: number;
+  name: string;
+
+  maximumDeepness: number;
+
+  numberOfLegs: number;
+  canSwim: boolean;
+  runningSpeed: number;
+}
+
+function buyAFish(
+  fishEntity: Pick<Animal, "age" | "name" | "maximumDeepness">
+) {
+  console.log(fishEntity);
+}
+
+buyAFish({
+  age: 1,
+  name: "Clown Fish",
+  maximumDeepness: 10,
+});
