1b0ba8603c7cfc984afd0b4027f99869bf8288f5
ShekarArun <shekar.arun@gmail.com>
2023-09-29 17:56:49 +0530

    feat: Add type intersections

---
+++ variables-advanced/typesIntersections.js
@@ -0,0 +1,4 @@
+// const inter1: T1 = { x: "x1", y: 2 }; // Won't compile: y does not exist in T1
+var inter2 = { x: "x1", y: 2 }; // Compile
+var inter3 = { x: "x1", y: 2 }; // Compile
+var with3Interfaces = { x: "1", y: 1, z: true };
---
+++ variables-advanced/typesIntersections.ts
@@ -0,0 +1,23 @@
+// The & symbol can be used to intersect types, which takes the combination of all properties of the specified types
+type T1 = { x: string };
+type T2 = { y: number };
+type T3 = { z: boolean };
+type IntersectType1 = T1 & T2 & T3;
+type IntersectType2 = T1 & T2;
+type IntersectType3 = T3 & T1;
+// const inter1: T1 = { x: "x1", y: 2 }; // Won't compile: y does not exist in T1
+const inter2: T1 & T2 = { x: "x1", y: 2 }; // Compile
+const inter3: IntersectType2 = { x: "x1", y: 2 }; // Compile
+
+// Interfaces can also be intersected
+interface I1 {
+  x: string;
+}
+interface I2 {
+  y: number;
+}
+interface I3 {
+  z: boolean;
+}
+type IntersectWithInterface = I1 & I2 & I3;
+const with3Interfaces: IntersectWithInterface = { x: "1", y: 1, z: true };
