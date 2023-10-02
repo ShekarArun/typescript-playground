e64b007b5d9a55bb09a6ff3f9abb717b0b0330fc
ShekarArun <shekar.arun@gmail.com>
2023-10-02 09:51:51 +0530

    feat: Add readonly tuple example

--- variables-advanced/tuple.ts
+++ variables-advanced/tuple.ts
@@ -7,3 +7,8 @@ let tuple1: [number, number] = [1, 2];
 let tuple2: [number, number, number] = [3, 4, 5];
 // tuple1 = tuple2; // Won't compile owing to length and property difference
 // tuple2 = tuple1; // Won't compile owing to length and property difference
+
+// A read only tuple can be created in the following manners
+let tuple3: readonly [number, number] = [1, 2];
+let tuple4: Readonly<[number, number]> = [1, 2];
+let tuple5 = [1, 2] as const;
